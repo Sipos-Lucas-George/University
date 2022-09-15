eedde5d6e9ecb8ffbfbe4b16d41f1dae215291c7
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2022-09-15 19:58:20 +0300

    Updated FP

--- Semester 1/Fundamentals of Programming/Practice/domain/gradeValidator.py
+++
@@ -1,8 +0,0 @@
-class GradeValidator:
-    def __init__(self, laboratoryNumber, laboratoryProblem, value):
-        if value < 1 or value > 10:
-            raise ValueError("Incorrect input!")
-        if laboratoryNumber < 1 or laboratoryNumber > 14:
-            raise ValueError("Incorrect input!")
-        if laboratoryProblem < 1 or laboratoryProblem > 20:
-            raise ValueError("Incorrect input!")
