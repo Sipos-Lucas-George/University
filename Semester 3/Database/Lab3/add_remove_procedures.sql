069ff56f5a5aba34ef4df0244ae52d73f8b5e1af
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2022-12-06 16:22:00 +0200

    Updated lab3

--- Semester 3/Database/Lab3/add_remove_procedures.sql
+++ Semester 3/Database/Lab3/add_remove_procedures.sql
@@ -9,68 +9,68 @@ CREATE OR ALTER PROCEDURE setTransportSupplierToInt AS
 go
 -- B
 -- add/remove a column
-CREATE PROCEDURE addCountryToLoadSupplier AS
+CREATE OR ALTER PROCEDURE addCountryToLoadSupplier AS
     ALTER TABLE LoadSupplier ADD country VARCHAR(30)
 go
 
-CREATE PROCEDURE removeCountryToLoadSupplier AS
+CREATE OR ALTER PROCEDURE removeCountryToLoadSupplier AS
     ALTER TABLE LoadSupplier DROP COLUMN country
 go
 
 -- C
 -- add/remove a default constraint
-CREATE PROCEDURE addDefaultConstraintLoadSupplier AS
+CREATE OR ALTER PROCEDURE addDefaultConstraintLoadSupplier AS
     ALTER TABLE LoadSupplier ADD CONSTRAINT DEFAULT0 DEFAULT('None') for country
 go
 
-CREATE PROCEDURE removeDefaultConstraintLoadSupplier AS
+CREATE OR ALTER PROCEDURE removeDefaultConstraintLoadSupplier AS
     ALTER TABLE LoadSupplier DROP CONSTRAINT DEFAULT0
 go
 
 -- G
-CREATE OR ALTER PROCEDURE addUselessTable AS
-    CREATE TABLE UselessTable (
+CREATE OR ALTER PROCEDURE addSecondDriver AS
+    CREATE TABLE SecondDriver (
         fullName VARCHAR(60) NOT NULL,
         sk VARCHAR(30) NOT NULL,
-        num INT NOT NULL,
+        tripID INT NOT NULL,
         CONSTRAINT USELESS_TABLE_PK PRIMARY KEY (fullName)
     )
 go
 
-CREATE PROCEDURE removeUselessTable AS
-    DROP TABLE UselessTable
+CREATE OR ALTER PROCEDURE removeSecondDriver AS
+    DROP TABLE SecondDriver
 go
 
 -- D
 -- add/remove a primary key
 CREATE OR ALTER PROCEDURE addSKasPK AS
-    ALTER TABLE UselessTable DROP CONSTRAINT IF EXISTS USELESS_TABLE_PK
-    ALTER TABLE UselessTable ADD CONSTRAINT USELESS_TABLE_PK PRIMARY KEY (fullName, sk)
+    ALTER TABLE SecondDriver DROP CONSTRAINT IF EXISTS USELESS_TABLE_PK
+    ALTER TABLE SecondDriver ADD CONSTRAINT USELESS_TABLE_PK PRIMARY KEY (fullName, sk)
 go
 
 CREATE OR ALTER PROCEDURE removeSKasPK AS
-    ALTER TABLE UselessTable DROP CONSTRAINT IF EXISTS USELESS_TABLE_PK
-    ALTER TABLE UselessTable ADD CONSTRAINT USELESS_TABLE_PK PRIMARY KEY (fullName)
+    ALTER TABLE SecondDriver DROP CONSTRAINT IF EXISTS USELESS_TABLE_PK
+    ALTER TABLE SecondDriver ADD CONSTRAINT USELESS_TABLE_PK PRIMARY KEY (fullName)
 go
 
 -- E
 -- add/remove candidate key
 CREATE PROCEDURE addCandidateKey AS
-    ALTER TABLE UselessTable ADD CONSTRAINT USELESS_CANDIDATE_KEY UNIQUE (num)
+    ALTER TABLE SecondDriver ADD CONSTRAINT USELESS_CANDIDATE_KEY UNIQUE (num)
 go
 
 CREATE OR ALTER PROCEDURE removeCandidateKey AS
-    ALTER TABLE UselessTable DROP CONSTRAINT IF EXISTS USELESS_CANDIDATE_KEY
+    ALTER TABLE SecondDriver DROP CONSTRAINT IF EXISTS USELESS_CANDIDATE_KEY
 go
 
 -- F
 -- add/remove foreign key
 CREATE PROCEDURE addForeignKey AS
-    ALTER TABLE UselessTable ADD CONSTRAINT USELESS_FOREIGN_KEY FOREIGN KEY (num) REFERENCES Trip(tripID)
+    ALTER TABLE SecondDriver ADD CONSTRAINT USELESS_FOREIGN_KEY FOREIGN KEY (num) REFERENCES Trip(tripID)
 go
 
 CREATE OR ALTER PROCEDURE removeForeignKey AS
-    ALTER TABLE UselessTable DROP CONSTRAINT IF EXISTS USELESS_FOREIGN_KEY
+    ALTER TABLE SecondDriver DROP CONSTRAINT IF EXISTS USELESS_FOREIGN_KEY
 go
 
 -- VERSION CONTROL
@@ -98,8 +98,8 @@ VALUES
     (3, 2, 'removeCountryToLoadSupplier'),
     (3, 4, 'addDefaultConstraintLoadSupplier'),
     (4, 3, 'removeDefaultConstraintLoadSupplier'),
-    (4, 5, 'addUselessTable'),
-    (5, 4, 'removeUselessTable'),
+    (4, 5, 'addSecondDriver'),
+    (5, 4, 'removeSecondDriver'),
     (5, 6, 'addSKasPK'),
     (6, 5, 'removeSKasPK'),
     (6, 7, 'addCandidateKey'),
@@ -107,13 +107,17 @@ VALUES
     (7, 8, 'addForeignKey'),
     (8, 7, 'removeForeignKey');
 
-CREATE PROCEDURE toVersion(@newVersion INT) AS
+CREATE OR ALTER PROCEDURE toVersion(@newVersion INT) AS
     DECLARE @curr INT
     DECLARE @var VARCHAR(MAX)
     SELECT @curr = version
     FROM versionTable
 
     IF @newVersion > (SELECT MAX(toVersion)
+                      FROM proceduresTable)
+        raiserror ('BAD VERSION', 10, 1)
+
+    IF @newVersion < (SELECT MIN(toVersion)
                       FROM proceduresTable)
         raiserror ('BAD VERSION', 10, 1)
 
