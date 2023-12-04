c93a878ed4f9f56e96f16693589c048edae52440
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2023-12-04 20:13:10 +0200

    lab_4: c# project
    Signed-off-by: Sipos-Lucas-George <lucas.sipos@yahoo.com>

---
+++ Semester 5/Parallel and Distributed Programming/Lab_4/Lab_4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Lab_4.Parser;
+using System.Collections.Generic;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        private static readonly List<string> Urls = new()
+        {
+            "www.dspcluj.ro/HTML/promovarea_sanatatii/promovare.html",
+            "filesamples.com/samples/code/html/sample2.html",
+            "filesamples.com/samples/code/html/sample1.html"
+        };
+
+        static void Main()
+        {
+            Console.WriteLine("1. Callback Parser");
+            Console.WriteLine("2. Task Parser");
+            Console.WriteLine("3. Async Await Parser");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    var callbackSolution = new CallbackSolution(Urls);
+                    break;
+                case "2":
+                    var taskSolution = new TaskSolution(Urls);
+                    break;
+                case "3":
+                    var asyncAwaitSolution = new AsyncAwaitSolution(Urls);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+}
